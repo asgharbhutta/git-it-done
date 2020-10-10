96fc7dac4de0e14f48cbbe6fa7462cd3ba38bfbe
AsgharB <abhutta@Asghar.local>
2020-10-10 12:43:09 -0400

    created elements

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,3 +1,5 @@
+var issuesContainer = document.querySelector("#issues-container");
+
 var getRepoIssues = function(repo){
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
@@ -6,7 +8,8 @@ var getRepoIssues = function(repo){
         console.log(response)
         if (response.ok) {
           response.json().then(function(data) {
-            console.log(data);
+              //pass response data to the DOM function
+            displayIssues(data);
           });
         }
         else {
@@ -16,6 +19,43 @@ var getRepoIssues = function(repo){
 
     }
 
+var displayIssues = function(issues){
+    for(x in issues){
+        if(issues.length === 0){
+            issuesContainer.textContent = "This repo has no open issues"
+            return;
+        }
+
+        var issueEl = document.createElement("a");
+        issueEl.classList = "list-item flex-row justify-space-between align-center";
+        issueEl.setAttribute("href", issues[x].html_url);
+        issueEl.setAttribute("target", "blank");
+
+        //create span to hold issue title
+        var titleEl = document.createElement("span");
+        titleEl.textContent = issues[x].title;
+
+        //append to container
+
+        issueEl.appendChild(titleEl);
+        
+        //create a type element
+        var typeEL = document.createElement("span");
+
+        //check to see if issue is an issue or pull request
+        if(issues[x].pull_request){
+            typeEL.textContent = "(Pull Request)";
+        }else{
+            typeEL.textContent = "(Issue)";
+        }
+
+        //append to container
+        issueEl.appendChild(typeEL);
+
+        issuesContainer.appendChild(issueEl);
+    }
+};
 
 
-getRepoIssues("facebook/react");
+
+getRepoIssues("microsoft");
